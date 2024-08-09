1b0eb02ea51faf4c8ea2121ce41a895bee6430df
MohammedMujahidAhmed <mujahid13131@gmail.com>
2024-08-09 14:44:34 +0530

    added mongoDB connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+//and isConnected?:number here ? is it can be empty or number
+type ConnectionObject = {
+    isConnected?:number
+}
+
+const connection : ConnectionObject = {}
+
+async function dbConnect() : Promise<void> {
+    if(connection.isConnected){
+        console.log("already connected to mongodb")
+        return;
+    }
+
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_URL || '',{})
+        connection.isConnected = db.connections[0].readyState
+        console.log('Db connected Successfully')
+    }catch(error){
+        console.log('Db connection failed')
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
