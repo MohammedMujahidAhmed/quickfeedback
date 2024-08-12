01af03063d34e3ac84dbd4ac6ad685d6953922a9
MohammedMujahidAhmed <mujahid13131@gmail.com>
2024-08-12 12:06:38 +0530

    made minor change in next-auth route

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,14 +19,17 @@ export async function middleware(request: NextRequest) {
     {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
+    if(!token && url.pathname.startsWith('/dashboard')){
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
 
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    // '/sign-in',
+    '/sign-in',
     '/sign-up',
     '/',
     '/dashboard/:path*',
